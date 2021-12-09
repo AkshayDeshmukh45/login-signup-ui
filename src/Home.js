40c3a854badbe72744ce9fbdcffc9efbef0fbeb4
Akshay Deshmukh <deshmukhaksh@outlook.com>
2021-12-09 11:21:05 +0530

    final

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 // import  "./image.jpg"
 function Home() {
-    return (
-        <div className="home"style={{margin:"20%", backgroundColor: 'green'}}>
-        {/* <img src={"./image.jpg"}/> */}
-            <div className="left"
-            style={{margin:"20% auto", backgroundColor: 'green'}}>
-
-            </div>
-            <div className=" btn" style={{display: 'flex', flexDirection:"row",justifyContent:"center"}}>
-            <button type="button" class="btn btn-outline-primary">Sign in</button>
-            <br></br>
-            <button style={{marginLeft:"5px"}} type="button" class="btn btn-outline-primary">Login</button>
-            </div>
-            <div className="right">
-            
-            </div>
+  return (
+    <div
+      className="home"
+      style={{ margin: "20%", backgroundColor: "lightgreen" }}
+    >
+      {/* <img src={"./image.jpg"}/> */}
+      <div className="left" style={{ marginLeft: "0%", marginTop: "5rem" }}>
+        <div
+          className=" btn"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <button type="button" class="btn btn-outline-primary">
+            Sign in
+          </button>
+          <br></br>
+          <button
+            style={{ marginLeft: "5px" }}
+            type="button"
+            class="btn btn-outline-primary"
+          >
+            Login
+          </button>
         </div>
-    )
+      </div>
+
+      <div className="right"></div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
